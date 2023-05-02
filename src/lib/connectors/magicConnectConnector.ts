7d12557ccb9c0900c9a193d7e36a0eb1eaceebb2
Srujan Gurram <srujangs8@gmail.com>
2023-05-03 00:26:55 +0530

    cleaned magicConnector code to remove redundancy and fixed error handling on connector function and simplified flow

--- src/lib/connectors/magicConnectConnector.ts
+++ src/lib/connectors/magicConnectConnector.ts
@@ -1,159 +1,159 @@
-import { Magic } from 'magic-sdk';
+import { Magic } from "magic-sdk";
 import {
-  InstanceWithExtensions,
-  MagicSDKAdditionalConfiguration,
-  SDKBase,
-} from '@magic-sdk/provider';
-import { ConnectExtension } from '@magic-ext/connect';
+	InstanceWithExtensions,
+	MagicSDKAdditionalConfiguration,
+	SDKBase,
+} from "@magic-sdk/provider";
+import { ConnectExtension } from "@magic-ext/connect";
 import {
-  Address,
-  Chain,
-  normalizeChainId,
-  UserRejectedRequestError,
-  Connector,
-} from '@wagmi/core';
-import { AbstractProvider } from 'web3-core';
-import { RPCProviderModule } from '@magic-sdk/provider/dist/types/modules/rpc-provider';
-import { ethers, Signer } from 'ethers';
-import { getAddress } from 'ethers/lib/utils.js';
+	Address,
+	Chain,
+	normalizeChainId,
+	UserRejectedRequestError,
+	Connector,
+} from "@wagmi/core";
+import { AbstractProvider } from "web3-core";
+import { RPCProviderModule } from "@magic-sdk/provider/dist/types/modules/rpc-provider";
+import { ethers, Signer } from "ethers";
+import { getAddress } from "ethers/lib/utils.js";
 
 // Define the interface for MagicConnector options
 export interface MagicConnectorOptions {
-  apiKey: string;
-  magicSdkConfiguration?: MagicSDKAdditionalConfiguration;
+	apiKey: string;
+	magicSdkConfiguration?: MagicSDKAdditionalConfiguration;
 }
 
 // MagicConnectConnector class extends the base wagmi Connector class
 export class MagicConnectConnector extends Connector {
-  readonly id = 'magic';
-  readonly name = 'Magic';
-  readonly ready = true;
-  provider: RPCProviderModule & AbstractProvider;
-  magic: InstanceWithExtensions<SDKBase, ConnectExtension[]>;
+	readonly id = "magic";
+	readonly name = "Magic";
+	readonly ready = true;
+	provider: RPCProviderModule & AbstractProvider;
+	magic: InstanceWithExtensions<SDKBase, ConnectExtension[]>;
 
-  // Constructor initializes the Magic instance
-  // allows connectUI modal to display faster when connect method is called
-  constructor(config: { chains?: Chain[]; options: MagicConnectorOptions }) {
-    super(config);
-    this.initializeMagicInstance();
-  }
+	// Constructor initializes the Magic instance
+	// allows connectUI modal to display faster when connect method is called
+	constructor(config: { chains?: Chain[]; options: MagicConnectorOptions }) {
+		super(config);
+		this.initializeMagicInstance();
+	}
 
-  // Private method to initialize the Magic instance
-  private initializeMagicInstance() {
-    const { apiKey, magicSdkConfiguration } = this.options;
-    if (typeof window !== 'undefined') {
-      this.magic = new Magic(apiKey, {
-        ...magicSdkConfiguration,
-        extensions: [new ConnectExtension()],
-      });
+	// Private method to initialize the Magic instance
+	private initializeMagicInstance() {
+		const { apiKey, magicSdkConfiguration } = this.options;
+		if (typeof window !== "undefined") {
+			this.magic = new Magic(apiKey, {
+				...magicSdkConfiguration,
+				extensions: [new ConnectExtension()],
+			});
 
-      this.provider = this.magic.rpcProvider;
-    }
-  }
+			this.provider = this.magic.rpcProvider;
+		}
+	}
 
-  // Connect method attempts to connects to wallet using Magic Connect modal
-  async connect() {
-    try {
-      await this.magic.wallet.connectWithUI();
-      const provider = await this.getProvider();
-      const chainId = await this.getChainId();
+	// Connect method attempts to connects to wallet using Magic Connect modal
+	async connect() {
+		try {
+			await this.magic.wallet.connectWithUI();
+			const provider = await this.getProvider();
+			const chainId = await this.getChainId();
 
-      this.registerProviderEventListeners(provider);
+			this.registerProviderEventListeners(provider);
 
-      const account = await this.getAccount();
+			const account = await this.getAccount();
 
-      return {
-        account,
-        chain: {
-          id: chainId,
-          unsupported: false,
-        },
-        provider,
-      };
-    } catch (error) {
-      throw new UserRejectedRequestError(error);
-    }
-  }
+			return {
+				account,
+				chain: {
+					id: chainId,
+					unsupported: false,
+				},
+				provider,
+			};
+		} catch (error) {
+			throw new UserRejectedRequestError(error);
+		}
+	}
 
-  // Private method to register event listeners for the provider
-  private registerProviderEventListeners(
-    provider: RPCProviderModule & AbstractProvider
-  ) {
-    if (provider.on) {
-      provider.on('accountsChanged', this.onAccountsChanged);
-      provider.on('chainChanged', this.onChainChanged);
-      provider.on('disconnect', this.onDisconnect);
-    }
-  }
+	// Private method to register event listeners for the provider
+	private registerProviderEventListeners(
+		provider: RPCProviderModule & AbstractProvider,
+	) {
+		if (provider.on) {
+			provider.on("accountsChanged", this.onAccountsChanged);
+			provider.on("chainChanged", this.onChainChanged);
+			provider.on("disconnect", this.onDisconnect);
+		}
+	}
 
-  // Disconnect method attempts to disconnect wallet from Magic
-  async disconnect(): Promise<void> {
-    try {
-      await this.magic.wallet.disconnect();
-      this.emit('disconnect');
-    } catch (error) {
-      console.error('Error disconnecting from Magic SDK:', error);
-    }
-  }
+	// Disconnect method attempts to disconnect wallet from Magic
+	async disconnect(): Promise<void> {
+		try {
+			await this.magic.wallet.disconnect();
+			this.emit("disconnect");
+		} catch (error) {
+			console.error("Error disconnecting from Magic SDK:", error);
+		}
+	}
 
-  // Get connected wallet address
-  async getAccount(): Promise<Address> {
-    const signer = await this.getSigner();
-    const account = await signer.getAddress();
-    return getAddress(account);
-  }
+	// Get connected wallet address
+	async getAccount(): Promise<Address> {
+		const signer = await this.getSigner();
+		const account = await signer.getAddress();
+		return getAddress(account);
+	}
 
-  // Get chain ID
-  async getChainId(): Promise<number> {
-    const networkOptions = this.options.magicSdkConfiguration?.network;
-    if (typeof networkOptions === 'object') {
-      const chainID = networkOptions.chainId;
-      if (chainID) return normalizeChainId(chainID);
-    }
-    throw new Error('Chain ID is not defined');
-  }
+	// Get chain ID
+	async getChainId(): Promise<number> {
+		const networkOptions = this.options.magicSdkConfiguration?.network;
+		if (typeof networkOptions === "object") {
+			const chainID = networkOptions.chainId;
+			if (chainID) return normalizeChainId(chainID);
+		}
+		throw new Error("Chain ID is not defined");
+	}
 
-  // Get the Magic Instance provider
-  async getProvider() {
-    if (!this.provider) {
-      this.provider = this.magic.rpcProvider;
-    }
-    return this.provider;
-  }
+	// Get the Magic Instance provider
+	async getProvider() {
+		if (!this.provider) {
+			this.provider = this.magic.rpcProvider;
+		}
+		return this.provider;
+	}
 
-  // Get the Magic Instance signer
-  async getSigner(): Promise<Signer> {
-    const provider = new ethers.providers.Web3Provider(
-      await this.getProvider()
-    );
-    return provider.getSigner();
-  }
+	// Get the Magic Instance signer
+	async getSigner(): Promise<Signer> {
+		const provider = new ethers.providers.Web3Provider(
+			await this.getProvider(),
+		);
+		return provider.getSigner();
+	}
 
-  // Autoconnect if account is available
-  async isAuthorized() {
-    try {
-      const walletInfo = await this.magic.wallet.getInfo();
-      return !!walletInfo;
-    } catch {
-      return false;
-    }
-  }
+	// Autoconnect if account is available
+	async isAuthorized() {
+		try {
+			const walletInfo = await this.magic.wallet.getInfo();
+			return !!walletInfo;
+		} catch {
+			return false;
+		}
+	}
 
-  // Event handler for accountsChanged event
-  onAccountsChanged = (accounts: string[]): void => {
-    if (accounts.length === 0) this.emit('disconnect');
-    else this.emit('change', { account: getAddress(accounts[0]) });
-  };
+	// Event handler for accountsChanged event
+	onAccountsChanged = (accounts: string[]): void => {
+		if (accounts.length === 0) this.emit("disconnect");
+		else this.emit("change", { account: getAddress(accounts[0]) });
+	};
 
-  // Event handler for chainChanged event
-  onChainChanged = (chainId: string | number): void => {
-    const id = normalizeChainId(chainId);
-    const unsupported = this.isChainUnsupported(id);
-    this.emit('change', { chain: { id, unsupported } });
-  };
+	// Event handler for chainChanged event
+	onChainChanged = (chainId: string | number): void => {
+		const id = normalizeChainId(chainId);
+		const unsupported = this.isChainUnsupported(id);
+		this.emit("change", { chain: { id, unsupported } });
+	};
 
-  // Event handler for disconnect event
-  onDisconnect = (): void => {
-    this.emit('disconnect');
-  };
+	// Event handler for disconnect event
+	onDisconnect = (): void => {
+		this.emit("disconnect");
+	};
 }
--- src/lib/connectors/magicConnector.ts
+++ src/lib/connectors/magicConnector.ts
@@ -1,121 +1,108 @@
-import { OAuthExtension, OAuthProvider } from '@magic-ext/oauth';
-import { InstanceWithExtensions, SDKBase } from '@magic-sdk/provider';
-import { RPCProviderModule } from '@magic-sdk/provider/dist/types/modules/rpc-provider';
-import { Address, Chain, Connector, normalizeChainId } from '@wagmi/core';
-import { ethers, Signer } from 'ethers';
-import { getAddress } from 'ethers/lib/utils';
-import { AbstractProvider } from 'web3-core';
+import { OAuthExtension, OAuthProvider } from "@magic-ext/oauth";
+import { InstanceWithExtensions, SDKBase } from "@magic-sdk/provider";
+import { Address, Chain, Connector, normalizeChainId } from "@wagmi/core";
+import { Signer, ethers } from "ethers";
+import { getAddress } from "ethers/lib/utils";
 
-import { createModal } from '../modal/view';
+import { createModal } from "../modal/view";
 
-const IS_SERVER = typeof window === 'undefined';
+const IS_SERVER = typeof window === "undefined";
 
 export interface MagicOptions {
-  apiKey: string;
-  accentColor?: string;
-  isDarkMode?: boolean;
-  customLogo?: string;
-  customHeaderText?: string;
+	apiKey: string;
+	accentColor?: string;
+	isDarkMode?: boolean;
+	customLogo?: string;
+	customHeaderText?: string;
 }
 
 interface UserDetails {
-  email: string;
-  phoneNumber: string;
-  oauthProvider: OAuthProvider;
+	email: string;
+	phoneNumber: string;
+	oauthProvider: OAuthProvider;
 }
 
 export abstract class MagicConnector extends Connector {
-  ready = !IS_SERVER;
+	ready = !IS_SERVER;
 
-  readonly id = 'magic';
+	readonly id = "magic";
 
-  readonly name = 'Magic';
+	readonly name = "Magic";
 
-  provider: RPCProviderModule & AbstractProvider;
+	isModalOpen = false;
 
-  isModalOpen = false;
+	magicOptions: MagicOptions;
 
-  magicOptions: MagicOptions;
+	protected constructor(config: { chains?: Chain[]; options: MagicOptions }) {
+		super(config);
+		this.magicOptions = config.options;
+	}
 
-  protected constructor(config: { chains?: Chain[]; options: MagicOptions }) {
-    super(config);
-    this.magicOptions = config.options;
-  }
+	async getAccount(): Promise<Address> {
+		const signer = await this.getSigner();
+		const account = await signer.getAddress();
+		if (account.startsWith("0x")) return account as Address;
+		return `0x${account}`;
+	}
 
-  async getAccount(): Promise<Address> {
-    const provider = new ethers.providers.Web3Provider(
-      await this.getProvider()
-    );
-    const signer = provider.getSigner();
-    const account = await signer.getAddress();
-    if (account.startsWith('0x')) return account as Address;
-    return `0x${account}`;
-  }
+	async getUserDetailsByForm(
+		enableSMSLogin: boolean,
+		enableEmailLogin: boolean,
+		oauthProviders: OAuthProvider[],
+	): Promise<UserDetails> {
+		const output: UserDetails = (await createModal({
+			accentColor: this.magicOptions.accentColor,
+			isDarkMode: this.magicOptions.isDarkMode,
+			customLogo: this.magicOptions.customLogo,
+			customHeaderText: this.magicOptions.customHeaderText,
+			enableSMSLogin: enableSMSLogin,
+			enableEmailLogin: enableEmailLogin || true,
+			oauthProviders,
+		})) as UserDetails;
 
-  async getUserDetailsByForm(
-    enableSMSLogin: boolean,
-    enableEmailLogin: boolean,
-    oauthProviders: OAuthProvider[]
-  ): Promise<UserDetails> {
-    const output: UserDetails = (await createModal({
-      accentColor: this.magicOptions.accentColor,
-      isDarkMode: this.magicOptions.isDarkMode,
-      customLogo: this.magicOptions.customLogo,
-      customHeaderText: this.magicOptions.customHeaderText,
-      enableSMSLogin: enableSMSLogin,
-      enableEmailLogin: enableEmailLogin || true,
-      oauthProviders,
-    })) as UserDetails;
+		this.isModalOpen = false;
+		return output;
+	}
 
-    this.isModalOpen = false;
-    return output;
-  }
+	async getProvider() {
+		const magic = this.getMagicSDK();
+		return new ethers.providers.Web3Provider(magic.rpcProvider);
+	}
 
-  async getProvider() {
-    if (this.provider) {
-      return this.provider;
-    }
-    const magic = this.getMagicSDK();
-    this.provider = magic.rpcProvider;
-    return this.provider;
-  }
+	async getSigner(): Promise<Signer> {
+		const provider = await this.getProvider();
+		const signer = provider.getSigner();
+		return signer;
+	}
 
-  async getSigner(): Promise<Signer> {
-    const provider = new ethers.providers.Web3Provider(
-      await this.getProvider()
-    );
-    const signer = await provider.getSigner();
-    return signer;
-  }
+	async isAuthorized() {
+		const magic = this.getMagicSDK();
+		try {
+			return await magic.user.isLoggedIn();
+		} catch (e) {
+			return false;
+		}
+	}
 
-  async isAuthorized() {
-    const magic = this.getMagicSDK();
-    try {
-      return await magic.user.isLoggedIn();
-    } catch (e) {
-      return false;
-    }
-  }
+	protected onAccountsChanged(accounts: string[]): void {
+		if (accounts.length === 0) this.emit("disconnect");
+		else this.emit("change", { account: getAddress(accounts[0]) });
+	}
 
-  protected onAccountsChanged(accounts: string[]): void {
-    if (accounts.length === 0) this.emit('disconnect');
-    else this.emit('change', { account: getAddress(accounts[0]) });
-  }
+	protected onChainChanged(chainId: string | number): void {
+		const id = normalizeChainId(chainId);
+		const unsupported = this.isChainUnsupported(id);
+		this.emit("change", { chain: { id, unsupported } });
+	}
 
-  protected onChainChanged(chainId: string | number): void {
-    const id = normalizeChainId(chainId);
-    const unsupported = this.isChainUnsupported(id);
-    this.emit('change', { chain: { id, unsupported } });
-  }
+	protected onDisconnect(): void {
+		this.emit("disconnect");
+	}
 
-  protected onDisconnect(): void {
-    this.emit('disconnect');
-  }
+	async disconnect(): Promise<void> {
+		const magic = this.getMagicSDK();
+		await magic.user.logout();
+	}
 
-  async disconnect(): Promise<void> {
-    const magic = this.getMagicSDK();
-    await magic.user.logout();
-  }
-
-  abstract getMagicSDK(): InstanceWithExtensions<SDKBase, OAuthExtension[]>;
+	abstract getMagicSDK(): InstanceWithExtensions<SDKBase, OAuthExtension[]>;
 }
